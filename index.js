317324d1314dfc0217ca0170bd6b4469850e1653
Peter Holló <lordpejdar@gmail.com>
2016-05-03 01:18:34 +0200

    add multiple log levels used to output different types of log messages

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
+var _ = require('lodash');
 var merge_descriptors = require('merge-descriptors');
 
 exports = module.exports = create;
+
+const levels = {
+  info: 'info',
+  log: 'log',
+  warn: 'warn',
+  error: 'error'
+};
+
+const separator = '::';
 
 /**
  * create bedug middlware
  * @param namespace
  * @param consumer
- * @return {i}
+ * @return {{
+ *  init: Function(namespace: String, consumer: Object),
+ *  info: Function,
+ *  log: Function,
+ *  warn: Function,
+ *  error: Function,
+ *  levels: Object,
+ *  separator: String
+ * }}
  */
 function create(namespace, consumer) {
   var i = function() {};
@@ -18,5 +36,13 @@ function create(namespace, consumer) {
 var bedug = {};
 
 bedug.init = function init(ns, co) {
-  this.log = co(ns);
+  var self = this;
+  if(_.has(co, 'name') && co.name == 'debug') {
+    _.each(levels, function(item, key) {
+      self[key] = co(item + separator + ns);
+    });
+  }
 };
+
+bedug.levels = levels;
+bedug.separator = separator;
--- test/debug.js
+++ test/debug.js
@@ -1,10 +1,11 @@
+var _ = require('lodash');
 var bedug = require('../.');
 var debug = require('debug');
 var intersect_stdout = require('intercept-stdout');
 var should = require('should');
 
 describe('create an middleware with \'debug\'', function() {
-  it('should use namespace', function(done) {
+  it('should use namespace', function() {
     var ns = 'test-namespace';
     var i = bedug(ns, debug);
     var output = '';
@@ -14,6 +15,20 @@ describe('create an middleware with \'debug\'', function() {
     i.log('testing namespace log');
     unhook();
     output.should.match(new RegExp(ns, 'g'));
-    done();
+  });
+  it('should use all log level', function() {
+    var ns = 'test-log-level';
+    var i = bedug(ns, debug);
+    var output = '';
+    var unhook = intersect_stdout(function(stdout) {
+      output += stdout;
+    });
+    var lvl = [];
+    _.each(i.levels, function(val) {
+      i[val]('testing log level log');
+      lvl.push(val + i.separator);
+    });
+    unhook();
+    output.should.match(new RegExp('(' + lvl.join('|') + ')', 'g'));
   });
 });
