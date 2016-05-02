e7e100753e1bcd470d16feba1ea5710cdb344a3b
Peter Holló <lordpejdar@gmail.com>
2016-05-03 00:30:09 +0200

    change creation test output DEBUG environment variable set to test-* to echo only test related output

--- test/create.js
+++ test/create.js
@@ -4,8 +4,8 @@ var should = require('should');
 
 describe('create middleware', function() {
   it('should create different \'debug\' middlware', function() {
-    var m1 = bedug('m1', debug);
-    var m2 = bedug('m2', debug);
+    var m1 = bedug('test-m1', debug);
+    var m2 = bedug('test-m2', debug);
     m1.should.not.be.equal(m2);
   });
 });
